a9c48d60c5c5ae9b8966d6b60d4649c38bf83d75
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-04-22 11:41:25 +0530

    refactor: Add logic to calculate even seconds

--- src/component/berlin-clock/index.js
+++ src/component/berlin-clock/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Seconds from "./seconds";
 import Constants from "../../constant";
+import PropTypes from "prop-types";
 
-const BerlinClock = () => {
+const BerlinClock = (props) => {
+  const second = props.time % 2 === 0 ? Constants.EVEN_SECOND : "";
+
   return (
     <div className="clock mv4">
-      <Seconds seconds={Constants.EVEN_SECOND} />
+      <Seconds seconds={second} />
     </div>
   );
+};
+
+BerlinClock.propTypes = {
+  time: PropTypes.string.isRequired,
 };
 
 export default BerlinClock;
--- src/test/berlin-clock/index.test.js
+++ src/test/berlin-clock/index.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import checkPropTypes from "check-prop-types";
 import BerlinClock from "../../component/berlin-clock";
 import Seconds from "../../component/berlin-clock/seconds";
 
@@ -7,7 +8,7 @@ describe("Berlin Clock component", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<BerlinClock />);
+    wrapper = shallow(<BerlinClock time={"00"} />);
   });
 
   it("should render a div", () => {
@@ -17,13 +18,41 @@ describe("Berlin Clock component", () => {
   it("should render the Seconds component", () => {
     expect(wrapper.find("Seconds").length).toEqual(1);
   });
+
+  it("Should throw error message if there is no time prop", () => {
+    const errorMsg =
+      "Failed prop type: The prop `time` is marked as required in `<<anonymous>>`, but its value is `undefined`.";
+
+    const result = checkPropTypes(
+      BerlinClock.propTypes,
+      { time: undefined },
+      "prop",
+      BerlinClock.time
+    );
+
+    expect(result).toEqual(errorMsg);
+  });
+
+  it("Should throw error message if time prop type is not a string", () => {
+    const errorMsg =
+      "Failed prop type: Invalid prop `time` of type `number` supplied to `<<anonymous>>`, expected `string`.";
+
+    const result = checkPropTypes(
+      BerlinClock.propTypes,
+      { time: 12 },
+      "prop",
+      BerlinClock.time
+    );
+
+    expect(result).toEqual(errorMsg);
+  });
 });
 
 describe("Seconds Lamp functionality", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<BerlinClock />);
+    wrapper = shallow(<BerlinClock time={"00"} />);
   });
 
   it("should pass Y for even second value", () => {
